57105d3e369a0df4afef058c9a9f38c05df89cae
Daniel <Daniel@DANI>
2015-03-16 09:22:01 +0100

    añadi comentarios explicativos

--- src/examencodtwitter/Metodos.java
+++ src/examencodtwitter/Metodos.java
@@ -14,49 +14,56 @@ import twitter4j.conf.ConfigurationBuilder;
  * @author Daniel
  */
 public class Metodos {
+
     Twitter twitter;
-
-     
-     public Metodos() {
-
-         ConfigurationBuilder cb = new ConfigurationBuilder();
-         cb.setDebugEnabled(true)
-                 .setOAuthConsumerKey("VnItYt2EEiPAymGQW01huMuXp")
-                 .setOAuthConsumerSecret("aE5Dygm2VbzfH9BokMGAvTEPhfIrBPOJX2nRvQFKId1t6rhLAa")
-                 .setOAuthAccessToken("779025319-KBKqeCiqBEXh0CqsvP8xL3wsSdmBWXEl2YGEqzQ4")
-                 .setOAuthAccessTokenSecret("WJIrTUkKi2LbXMYsax0mRDcxz5PC1olzXKyVzAGTnZVfJ");
-
-         twitter = new TwitterFactory(cb.build()).getInstance();
-
-     }
-     
-      public void verTL() throws TwitterException {
-
-         List<Status> statuses = twitter.getHomeTimeline();
-         System.out.println("Showing home timeline.");
-         for (Status status : statuses) {
-             System.out.println(status.getUser().getName() + ":"
-                     + status.getText());
-         }
-
-     }
-      public void buscarTrending(String busqueda) throws TwitterException {
-
-         Query query = new Query(busqueda);
-         QueryResult result = twitter.search(query);
-         for (Status status : result.getTweets()) {
-             System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-         }
-
-     }
-      public void tweetear(String tweet) throws TwitterException {
-
-         Status status = twitter.updateStatus(tweet);
-         System.out.println("Successfully updated the status to [" + status.getText() + "].");
-
-     }
-      
-     
-
     
+    /* El constructor da acceso a la clase */
+
+    public Metodos() {
+
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("VnItYt2EEiPAymGQW01huMuXp")
+                .setOAuthConsumerSecret("aE5Dygm2VbzfH9BokMGAvTEPhfIrBPOJX2nRvQFKId1t6rhLAa")
+                .setOAuthAccessToken("779025319-KBKqeCiqBEXh0CqsvP8xL3wsSdmBWXEl2YGEqzQ4")
+                .setOAuthAccessTokenSecret("WJIrTUkKi2LbXMYsax0mRDcxz5PC1olzXKyVzAGTnZVfJ");
+
+        twitter = new TwitterFactory(cb.build()).getInstance();
+
+    }
+    
+    /*Método para ver el TimeLine de tu Twitter */
+
+    public void verTL() throws TwitterException {
+
+        List<Status> statuses = twitter.getHomeTimeline();
+        System.out.println("Showing home timeline.");
+        for (Status status : statuses) {
+            System.out.println(status.getUser().getName() + ":"
+                    + status.getText());
+        }
+
+    }
+    
+    /*Método para buscar una string */
+
+    public void buscarTrending(String busqueda) throws TwitterException {
+
+        Query query = new Query(busqueda);
+        QueryResult result = twitter.search(query);
+        for (Status status : result.getTweets()) {
+            System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+        }
+
+    }
+    
+    /*Metodo para twitear */
+
+    public void tweetear(String tweet) throws TwitterException {
+
+        Status status = twitter.updateStatus(tweet);
+        System.out.println("Successfully updated the status to [" + status.getText() + "].");
+
+    }
+
 }
