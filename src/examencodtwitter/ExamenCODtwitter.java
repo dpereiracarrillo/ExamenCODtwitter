5441ad64aa40a5a71112cc3cf4d3bdf330976b64
Daniel <Daniel@DANI>
2015-03-16 09:13:34 +0100

    llame a los metodos desde el main

--- src/examencodtwitter/ExamenCODtwitter.java
+++ src/examencodtwitter/ExamenCODtwitter.java
@@ -1,5 +1,8 @@
 package examencodtwitter;
 
+import javax.swing.JOptionPane;
+import twitter4j.TwitterException;
+
 /**
  *
  * @author Daniel
@@ -7,10 +10,19 @@ package examencodtwitter;
 public class ExamenCODtwitter {
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TwitterException {
         String busqueda;
          String tweet;
          int opcion;
+         Metodos obj = new Metodos();
+
+         obj.verTL();
+
+          busqueda = JOptionPane.showInputDialog("Introduzca lo que desee buscar en Twitter");
+          obj.buscarTrending(busqueda);
+
+          tweet = JOptionPane.showInputDialog("Introduzca lo que desee twittear");
+          obj.tweetear(tweet);
     }
     
 }
